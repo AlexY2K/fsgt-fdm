5b3ccdf29f008c473c12483be5aceb8609662211
Alexandre NGUYEN TRUNG <alexrush@gmail.com>
2026-02-06 02:12:12 +0100

    chore: add shared components and utilities
    - Themed, StyledText, EditScreenInfo, ExternalLink
    - useColorScheme, useClientOnlyValue (web/native)
    - StyledText test, VSCode config

---
+++ components/__tests__/StyledText-test.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react-native';
+
+import { MonoText } from '../StyledText';
+
+it('renders correctly', () => {
+  render(<MonoText>Snapshot test!</MonoText>);
+  expect(screen.getByText('Snapshot test!')).toBeTruthy();
+});
